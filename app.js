ccc7e8a79ce21d4cd5f8880086b032c81ad4bab7
Reeba12 <reebasiddiqui456@gmail.com>
2022-09-27 12:47:21 -0700

    completed

---
+++ app.js
@@ -0,0 +1,21 @@
+function squr(){
+    var div = document.createElement('div')
+    div.setAttribute("class","shap")
+    document.body.appendChild(div);
+   
+}
+function req(){
+    var div = document.createElement('div')
+    div.setAttribute("class","recg")
+    document.body.appendChild(div);
+}
+function trian(){
+    var div = document.createElement('div')
+    div.setAttribute("class","tri")
+    document.body.appendChild(div);
+}
+function cir(){
+    var div = document.createElement('div')
+    div.setAttribute("class","circl")
+    document.body.appendChild(div);
+}
